6fe81783439e4cdb19de012ba40ab918fc6f552e
aaa <aaa>
2026-05-28 19:46:11 +0900

    敵が出るようにした。 当たり判定を作成中。

--- SmartPhoneProject_Android/app/src/main/cpp/week1.cpp
+++ SmartPhoneProject_Android/app/src/main/cpp/week1.cpp
@@ -4,6 +4,38 @@
 int screenW = 720;  //画面の幅
 int screenH = 1280; //画面の高さ
 
+struct SpawnPoint
+{
+    int x;
+    int y;
+};
+
+SpawnPoint spawn[3] =
+        {
+                {screenW / 2, 0},
+                {100, 0},
+                {screenW - 100, 0},
+
+        };
+
+///プレイヤーが移動可能な座標の構造体
+struct MovePoint
+{
+    int x;
+    int y;
+};
+
+MovePoint points[3] =
+        {
+                {screenW / 2, screenH - 200},
+                {100, screenH - 200},
+                {screenW - 100, screenH -200}
+        };
+
+//////////////////////////////////////////
+//  タッチ操作関係
+//////////////////////////////////////////
+
 ///タッチ入力の座標を表す構造体
 struct TouchPoint
 {
@@ -102,6 +134,7 @@ void UpdateMobileInput(MobileInput* input)
                     (float)input->currentY
             );
 
+            //経過時間の計算
             int elapsedTime = GetNowCount() - input->startTime;
 
             if(distance < swipeDistance)
@@ -124,27 +157,30 @@ void UpdateMobileInput(MobileInput* input)
         }
     }
 
-    //タッチ中の座標を更新
-    int x = 0;
-    int y = 0;
-
-    GetTouchInput(0,&x, &y,NULL, NULL);
-
-    if(!input->isTouching)
+    if(touchNum > 0)
     {
-        input->isTouching = true;
+        //タッチ中の座標を更新
+        int x = 0;
+        int y = 0;
 
-        input->startX = x;
-        input->startY = y;
-        input->currentX = x;
-        input->currentY = y;
+        GetTouchInput(0,&x, &y,NULL, NULL);
 
-        input->startTime = GetNowCount();
-    }
-    else
-    {
-        input->currentX = x;
-        input->currentY = y;
+        if(!input->isTouching)
+        {
+            input->isTouching = true;
+
+            input->startX = x;
+            input->startY = y;
+            input->currentX = x;
+            input->currentY = y;
+
+            input->startTime = GetNowCount();
+        }
+        else
+        {
+            input->currentX = x;
+            input->currentY = y;
+        }
     }
 
     //ピンチ処理(指が2本の時)
@@ -189,6 +225,83 @@ void UpdateMobileInput(MobileInput* input)
     }
 }
 
+//////////////////////////////////////////////
+//  エネミー関係
+//////////////////////////////////////////////
+
+///エネミーの構造体
+struct Enemy
+{
+    int x;
+    int y;
+    int width;
+    int height;
+    bool active;
+};
+
+const int Max_Enemies = 2;
+Enemy enemies[Max_Enemies];
+
+///エネミーを生成
+void SpawnEnemy()
+{
+    for(int i = 0; i < Max_Enemies; i++)
+    {
+        if(!enemies[i].active)
+        {
+            int index = rand() % 3;
+
+            enemies[i].x = spawn[index].x;
+            enemies[i].y = spawn[index].y + 200;
+            enemies[i].width = 50;
+            enemies[i].height = 50;
+
+            enemies[i].active = true;
+
+            break;
+        }
+    }
+}
+
+void DrawEnemy()
+{
+    for(int i = 0; i < Max_Enemies; i++)
+    {
+        if(!enemies[i].active) continue;
+
+        DrawBox(
+                enemies[i].x - enemies[i].width,
+                enemies[i].y - enemies[i].height,
+                enemies[i].x + enemies[i].width,
+                enemies[i].y + enemies[i].height,
+                GetColor(255,0,0),
+                true
+                );
+    }
+}
+
+void EnemyUpdate()
+{
+    for(int i = 0; i < Max_Enemies; i++)
+    {
+        //
+        if(!enemies[i].active) continue;
+
+        //下方向に移動
+        enemies[i].y += 6.0f;
+
+        if(enemies[i].y > 1350)
+        {
+            enemies[i].active = false;
+            SpawnEnemy();
+        }
+    }
+}
+
+///////////////////////////////////////////////
+//  プレイヤー関係
+///////////////////////////////////////////////
+
 ///プレイヤーの構造体
 struct Player
 {
@@ -200,17 +313,17 @@ struct Player
 ///プレイヤーのサイズを初期化
 void InitPlayer(Player* player)
 {
-    player->x = screenW / 2 - 50;
+    player->x = screenW / 2;
     player->y = screenH - 200;
-    player->size = 100;
+    player->size = 50;
 }
 
 ///プレイヤーを表示(Box)
 void PlayerDraw(const Player* player)
 {
     DrawBox(
-            player->x,
-            player->y,
+            player->x - player->size,
+            player->y - player->size,
             player->x + player->size,
             player->y + player->size,
             GetColor(0,0,255),
@@ -218,12 +331,30 @@ void PlayerDraw(const Player* player)
             );
 }
 
+///プレイヤーの移動処理
 void PlayerControl(Player* player, MobileInput* input)
 {
+    //タップした時
     if(input->tap)
     {
         player->x = input->currentX;
-        player->y = input->currentY;
+        int nearestIndex = 0;
+        int nearestDistance = abs(player->x - points[0].x);
+
+        for(int i = 0; i < 3; i++)
+        {
+            //目標地点がタップ位置からどれくらい離れているか計算
+            int distance = abs(player->x - points[i].x);
+            if(distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                nearestIndex = i;
+            }
+        }
+
+        //一番近い移動ポイントに移動
+        player->x = points[nearestIndex].x;
+        player->y = points[nearestIndex].y;
     }
 }
 
@@ -234,6 +365,42 @@ void Draw()
     DrawLine(100, 0, 100, screenH, GetColor(255,255,255), 0);
     DrawLine(screenW / 2, 0, screenW / 2, screenH, GetColor(255,255,255), 0);
     DrawLine(screenW - 100, 0, screenW - 100, screenH, GetColor(255,255,255), 0);
+}
+
+///当たり判定の関数
+bool CheckHit(int x1, int y1, int size1, int x2, int y2, int size2)
+{
+    int half1 = size1 / 2;
+    int half2 = size2 / 2;
+
+    if(x1 + half1 < x2 - half2) return false;//1が左
+    if(x1 - half1 > x2 + half2) return false;//1が右
+    if(y1 + half1 < y2 - half2) return false;//1が上
+    if(y1 - half1 > y2 - half2) return false;//1が下
+
+    //当たっている
+    return true;
+}
+
+Player player;
+MobileInput input;
+MovePoint point;
+
+//ゲームのUpdate
+void UpdateGame()
+{
+    UpdateMobileInput(&input);
+    PlayerControl(&player, &input);
+    EnemyUpdate();
+    SpawnEnemy();
+}
+
+//ゲームのDraw
+void DrawGame()
+{
+    DrawEnemy();
+    Draw();
+    PlayerDraw(&player);
 }
 
 int android_main()
@@ -250,22 +417,15 @@ int android_main()
     //裏画面に表示するようにする
     SetDrawScreen(DX_SCREEN_BACK);
 
-    Player player;
-    MobileInput input;
-
     InitPlayer(&player);
     InitMobileInput(&input);
 
     //メインループ
     while(ProcessMessage() == 0)
     {
+        UpdateGame();
         ClearDrawScreen();
-
-        Draw();
-        UpdateMobileInput(&input);
-
-        PlayerDraw(&player);
-        PlayerControl(&player, &input);
+        DrawGame();
         ScreenFlip();
     }
 
