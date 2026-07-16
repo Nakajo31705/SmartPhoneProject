cab1a013fb86f4fb10df336cdafed04b8751469b
qa <>
2026-07-16 19:44:59 +0900

    アセットの追加、インプット処理の導入、プレイヤーの作成開始

---
+++ OneOnOne_SmartPhone/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private MobileInputVisualizer input;
+    [SerializeField]
+    private int hp;
+    [SerializeField]
+    private int power;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
